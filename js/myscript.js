4ecc789816b2ecd8ececba09ddcd665ae2cc9dc1
sergey <chernushov88@mail.ru>
2015-11-02 19:17:50 +0300

    providers.html company-master.html

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,35 @@
 ﻿
 /*-----------------------------------------------------------------------------------*/
-/*	
+/*	accordion
 /*-----------------------------------------------------------------------------------*/
 
+$(document).ready(function(){			
+				$(".accordion h3:first").addClass("");
+				$(".accordion ul.menu-body:not(:first)").hide();
 
+				$(".accordion h3").click(function(){
+					$(this).next("ul.menu-body").slideToggle("slow")
+					.siblings("ul.menu-body:visible").slideUp("slow");
+					$(this).toggleClass("active");
+					$(this).siblings("h3").removeClass("active");
+				});
+			});
+/*-----------------------------------------------------------------------------------*/
+/*	menu
+/*-----------------------------------------------------------------------------------*/
+$(document).ready(function(){
+	var touch = $('#touch-menu');
+	var menu = $('.sidebar-inside');
+
+	$(touch).on('click', function(e) {
+	  e.preventDefault();
+	  menu.slideToggle();
+	});
+	$(window).resize(function(){
+
+	  if(menu.is(':hidden')) {
+	    	menu.removeAttr('style');
+	  }
+	});
+	});
 
